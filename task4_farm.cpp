82639c78d0ece5051d6c571bac2d57581dc31688
kebrlej <s.kebrle@gmail.com>
2019-11-30 17:32:03 +0100

    Task 4 farm finished.

--- task4_farm.cpp
+++ task4_farm.cpp
@@ -1,26 +1,38 @@
 #include "common.hpp"
 
-void M_emittor(){
+void M_emittor(int first, int last){
 	streamelement x;
+	int sendWorkTo = first;
 	while(true) {
 		x.recv(0);
 		if(x.is_terminated()) {
-			x.send(2);
+			for(int i = first; i<=last; i++){		
+				x.send(i);	
+			}
 			return;
 		}
-		x.send(2);
+		x.send(sendWorkTo);
+
+		sendWorkTo++;
+		if(sendWorkTo > last){
+			sendWorkTo = first;
+		}
 	}
 }
 
-void M_collector(){
+void M_collector(int numberOfWorkers){
 	streamelement x;
+	int numberOfTerminated = 0;
 	while(true) {
 		x.recv_any();
 		if(x.is_terminated()) {
-			x.send(4);
-			return;
+			numberOfTerminated++;
+			if(numberOfTerminated == numberOfWorkers){
+				x.send(7);
+				return;	
+			}
 		}
-		x.send(4);
+		x.send(7);
 	}
 }
 
@@ -29,17 +41,17 @@ void M_worker() {
 	while(true) {
 		x.recv(1);
 		if(x.is_terminated()) {
-			x.send(3);
+			x.send(6);
 			return;
 		}
 		for(int j=0; j<STREAM_ELEMENT_SIZE; ++j)
 			x[j] = g1(f1(x[j]));
-		x.send(3);
+		x.send(6);
 	}
 }
 
 
-#define REQUIRED_COMM_SIZE 5
+#define REQUIRED_COMM_SIZE 8
 
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
@@ -49,11 +61,11 @@ int main(int argc, char** argv) {
 		MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
 		double start = get_time(MPI_COMM_WORLD);
 		if(rank==0) M_first(1);
-		if(rank==1) M_emittor();
+		if(rank==1) M_emittor(2,5);
 		
 		if(rank>=2 && rank <= 5) M_worker();
 
-		if(rank==6) M_collector();
+		if(rank==6) M_collector(4);
 		if(rank==7) M_last(6);
 		double end = get_time(MPI_COMM_WORLD);
 		if(rank==0) printf("elapsed time = %.2f seconds\n", end-start);
